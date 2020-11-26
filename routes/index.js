240028ac595deaa51a78dafeb85dd6eb20681795
Arrasyidff <aff.anton20@gmail.com>
2020-11-27 01:14:50 +0800

    API PetFind Create Delete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const router = require('express').Router()
+const { response } = require('express')
 // const catRouter = require('./cat')
 const {UserController} = require('../controller')
+const {CatController} = require('../controller')
 
 // router.use('/cat', catRouter)
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+
+router.get("/catsphoto", CatController.getPhotoCats)
+router.get("/catsfact", CatController.getCatFacts)
+
+router.get("/cats", CatController.getPetFinder)
+router.post("/cats/:id", CatController.adoptCats)
+router.delete("/cats/:id", CatController.deleteCats)
+
 
 module.exports = router
