d656b4e46d091a15fc5ecc6b6f2092e573a9551e
Arrasyidff <aff.anton20@gmail.com>
2020-11-27 11:41:22 +0800

    menambah routes dan static method

--- routes/cat.js
+++ routes/cat.js
@@ -4,6 +4,7 @@ const authentication = require('../middleware/authentication')
 
 router.use(authentication)
 router.get("/", CatController.getPetFinder)
+router.get("/:id", CatController.getPetFinderById) // Tambahan
 router.post("/:id", CatController.adoptCats)
 
 module.exports = router
